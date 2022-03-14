7d7bd529dbd44e39e92713d20583b52ee738eddc
Valentin <valentin.paes0@gmail.com>
2022-03-14 16:24:28 -0300

    Update table view with styles

--- app/models/element.rb
+++ app/models/element.rb
@@ -1,7 +1,11 @@
 class Element < ApplicationRecord
-  belongs_to :category, class_name: "ElementCategory", foreign_key: "element_category_id"
+  belongs_to :category, class_name: "ElementCategory", foreign_key: "element_category_id", optional: true
 
   enum phase: {solid: 'Solid', liquid: 'Liquid', gas: 'Gas'}
 
   validates :number, :uniqueness =>  true
+
+  def self.find_by_position(x, y)
+    find_by(xpos: x, ypos: y)
+  end
 end
