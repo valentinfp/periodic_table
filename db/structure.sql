9b25c429b9eba94a6b6325516660d0dcce23be1f
Valentin Paes <valentin.paes0@gmail.com>
2022-03-14 21:50:03 +0000

    Try to configure gitpod

--- db/structure.sql
+++ db/structure.sql
@@ -1,5 +1,6 @@
 SET statement_timeout = 0;
 SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
@@ -7,20 +8,6 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
-
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
 
 --
 -- Name: element_phase; Type: TYPE; Schema: public; Owner: -
@@ -35,7 +22,7 @@ CREATE TYPE public.element_phase AS ENUM (
 
 SET default_tablespace = '';
 
-SET default_with_oids = false;
+SET default_table_access_method = heap;
 
 --
 -- Name: ar_internal_metadata; Type: TABLE; Schema: public; Owner: -
@@ -146,21 +133,21 @@ CREATE TABLE public.schema_migrations (
 
 
 --
--- Name: id; Type: DEFAULT; Schema: public; Owner: -
+-- Name: element_categories id; Type: DEFAULT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.element_categories ALTER COLUMN id SET DEFAULT nextval('public.element_categories_id_seq'::regclass);
 
 
 --
--- Name: id; Type: DEFAULT; Schema: public; Owner: -
+-- Name: elements id; Type: DEFAULT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.elements ALTER COLUMN id SET DEFAULT nextval('public.elements_id_seq'::regclass);
 
 
 --
--- Name: ar_internal_metadata_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+-- Name: ar_internal_metadata ar_internal_metadata_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.ar_internal_metadata
@@ -168,7 +155,7 @@ ALTER TABLE ONLY public.ar_internal_metadata
 
 
 --
--- Name: element_categories_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+-- Name: element_categories element_categories_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.element_categories
@@ -176,7 +163,7 @@ ALTER TABLE ONLY public.element_categories
 
 
 --
--- Name: elements_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+-- Name: elements elements_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.elements
@@ -184,7 +171,7 @@ ALTER TABLE ONLY public.elements
 
 
 --
--- Name: schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+-- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.schema_migrations
@@ -206,7 +193,7 @@ CREATE INDEX index_elements_on_phase ON public.elements USING btree (phase);
 
 
 --
--- Name: fk_rails_6dc21032b4; Type: FK CONSTRAINT; Schema: public; Owner: -
+-- Name: elements fk_rails_6dc21032b4; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.elements
